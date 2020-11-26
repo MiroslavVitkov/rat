cc5911185dff83e594737d08b53103ffcc75466c
Miroslav Vitkov <sir.vorac@gmail.com>
2020-11-26 18:37:05 +0200

    [py] Proof of concept on 'localhost'.

--- python/rat.py
+++ python/rat.py
@@ -7,5 +7,37 @@ Refer to the README for dessign goals and usage.
 '''
 
 
+import socket
+
+
+def create_server(port):
+    '''
+    Anticipate connections. Mind your firewall.
+    '''
+    s = socket.create_server(('', port))
+    s.listen()
+    conn, addr = s.accept()
+    print('New connection from', addr)
+    # Err: Transport endpoint is not conected!
+    data = conn.recv(1024)
+    print('READ:', data)
+
+
+def create_client(ip, port):
+    s = socket.create_connection((ip, port))
+    s.sendall(b'Heya')
+
+
 if __name__ == '__main__':
-    print('Hello World')
+    import threading
+    port = 42666
+
+    threading.Thread(target=create_server
+                    , args=[port]
+                    , name='server'
+                    ).start()
+
+    threading.Thread(target=create_client
+                    , args=['localhost', port]
+                    , name='client'
+                    ).start()
