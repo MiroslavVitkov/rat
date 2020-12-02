1cfa3a9dbc7555ce33aa1cc9de01b2f2389dea72
Miroslav Vitkov <sir.vorac@gmail.com>
2020-12-02 09:21:56 +0200

    [py] Socket testing no longer hangs forever.

--- python/sock.py
+++ python/sock.py
@@ -49,7 +49,9 @@ class Server:
                 conn, addr = s.accept()    # conn - socket.socket to a peer
                 print('New client connected:', addr)
                 t.map(func, [conn])
-
+                if True:
+                    return
+                time.sleep(0.1)
 
 class Client:
     def __init__(me, ip: str, port: int, func: callable):
@@ -60,30 +62,44 @@ class Client:
 def test():
     port = 42666
 
+    # Thread handlers are hard to write.
+    # It would be nice to incorporate most of the complexity into the class.
     def listen(s):
+        timeout = 0
         while True:
             time.sleep(0.1)
             d = s.recv(1024)
             if d:
                 print('received', len(d), 'bytes:', d)
+            timeout += 1
+            if timeout > 20:
+                return
 
     def yell(s):
         for i in range(0, 20):
             msg = 'This is the' + str(i) + 'th message!'
             s.sendall(msg.encode('utf8'))
 
-    threading.Thread(target=Server
-                    , args=[port, listen]
-                    , name='server'
-                    ).start()
-    threading.Thread(target=Client
-                    , args=['localhost', port, yell]
-                    , name='client0'
-                    ).start()
-    threading.Thread(target=Client
-                    , args=['localhost', port, yell]
-                    , name='client1'
-                    ).start()
+    # Internally starts a new thread per client.
+    ts = threading.Thread(target=Server
+                         , args=[port, listen]
+                         , name='server'
+                         )
+    tc0 = threading.Thread(target=Client
+                          , args=['localhost', port, yell]
+                          , name='client0'
+                          )
+    tc1 = threading.Thread(target=Client
+                          , args=['localhost', port, yell]
+                          , name='client1'
+                          )
+
+    ts.start()
+    tc0.start()
+    tc1.start()
+    tc0.join()
+    tc1.join()
+    ts.join()
 
 
 if __name__ == '__main__':
