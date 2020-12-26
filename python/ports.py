4defed3d10834a4baf1d6a25e0a7a2a663c530fe
Miroslav Vitkov <sir.vorac@gmail.com>
2020-12-26 12:12:34 +0200

    [py] Ports definition.

---
+++ python/ports.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+'''
+Standard ports used by the program.
+'''
+
+
+CHATSERVER = 42666
+NAMESERVER = CHATSERVER + 1
+
+
+if __name__ == '__main__':
+    print('rat listens for peers on', CHATSERVER,
+          'and provides names on', NAMESERVER)
