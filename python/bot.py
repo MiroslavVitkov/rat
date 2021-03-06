3ec0f8bc2f52e841d8458efd615b302e7c0e0a48
Miroslav Vitkov <sir.vorac@gmail.com>
2021-03-06 12:15:56 +0200

    [py] bot.py lightly refactored.

--- python/bot.py
+++ python/bot.py
@@ -5,9 +5,9 @@ A demo of how to script the client.
 '''
 
 
-import rat
-
 import random
+import time
+
 
 # synonims of villain:
 # miscreant blackguard criminal lawbreaker outlaw offender felon convict jailbird malefactor wrongdoer black hat supervillain transgressor sinner gangster gunman bandit brigand desperado thief robber mugger swindler fraudster racketeer terrorist pirate rogue scoundrel wretch heel reprobate charlatan evil-doer ruffian hoodlum hooligan thug delinquent ne'er-do-well good-for-nothing malfeasant misfeasor infractor cad knave rake crook con baddie shark rat snake snake in the grass dog hound louse swine scumbag wrong 'un crim rotter bounder bad egg stinker
@@ -30,12 +30,16 @@ def curse():
 
 
 def relay():
+    '''A chatroom.'''
     pass
 
 
 def log():
+    '''A chat history logging utility.'''
     pass
 
 
 if __name__ == '__main__':
-    pass
+    for i in range(10):
+        print(curse())
+        time.sleep(random.randint(8, 60))
