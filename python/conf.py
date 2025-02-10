5445a1a9228a66e34c5827cc8862eff9cb5d51c2
Miroslav Vitkov <sir.vorac@gmail.com>
2025-02-10 14:40:39 +0200

    [py] class User moved to conf. C-tor temporarily maimed due to spagetti dependencies.

--- python/conf.py
+++ python/conf.py
@@ -39,9 +39,64 @@ def get(path: str=where_are_we()+'/../conf.ini', c: list=[]):
 
     return c[0]
 
+
 def get_keypath():
     p = get()['user']['keypath']
     return os.path.expanduser(p)
+
+
+class User:
+    '''
+    A 'user' is defined by their public key.
+    Meaning 5 devices with the same key - one logical user.
+    rat is supposed to allow different users to perform code paths in a single invocation.
+    '''
+    U = get()['user']
+
+
+    def __init__( me
+                , name: str=U['name']
+                , group: str=U['group']
+                , pub=None
+                , ips: [str]=[]
+#                , pub: crypto.Pub=crypto.read_keypair()[1]
+#                , ips: [str]=[sock.get_extern_ip()]
+                , status: str=U['status']
+                ):
+        me.name = name
+        me.group = group
+        me.pub = pub
+        me.ips = ips
+        me.status = status
+
+
+    @classmethod
+    def from_bytes(cls, b: bytes) -> 'User':
+        obj = pickle.loads(b)
+        assert type(obj) == cls, (type(obj), cls)
+        return obj
+
+
+    def to_bytes(me) -> bytes:
+        return pickle.dumps(me)
+
+
+    def __repr__(me) -> str:
+        assert(me)
+        assert(me.pub)
+        pub = str(me.pub.save_pkcs1())
+        ips = ', '.join(me.ips)
+        return ( '\n'
+               + 'nickname: ' + me.name + '\n'
+               + 'group: ' + me.group + '\n'
+               + 'public key: ' + pub + '\n'
+               + 'IPs: ' + ips + '\n'
+               + 'status: ' + me.status)
+
+    def __eq__(me, other):
+        if type(me) != type(other):
+            return False;
+        return me.__dict__ == other.__dict__
 
 
 def test():
