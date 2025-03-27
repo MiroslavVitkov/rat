c4d8bc8b1210c022fdc16254aa5470187c144c2c
Miroslav Vitkov <sir.vorac@gmail.com>
2025-03-27 04:22:37 +0200

    [py] Trying to figure out how to get encoded frames and an API for that.

--- python/video.py
+++ python/video.py
@@ -2,24 +2,50 @@
 
 # In this file: reading frames one by one from the webcam.
 
+
 import ffmpeg
 
 
+### Public API.
+
+# cb(frame) - called on each frame
+def run(cb):
+    return None
+
+
+### Details.
 width = 640
 height = 480
 
 reader = (
     ffmpeg
     .input('/dev/video0', s='{}x{}'.format(width, height))
-    .output('pipe:', format='rawvideo', pix_fmt='rgb24')
-    .overwrite_output()
+    .output('pipe:', format='rawvideo', pix_fmt='yuv420p')
     .run_async(pipe_stdout=True)
 )
 
+
+encoder = (
+    ffmpeg
+    .input('pipe:', format='rawvideo', pix_fmt='yuv420p', s='{}x{}'.format(width, height))
+    .output('pipe:', format='h264', pix_fmt='yuv420p')
+    .run_async(pipe_stdin=True, pipe_stdout=True)
+)
+
+
 writer = (
     ffmpeg
-    .input('pipe:', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width, height))
-    .output('/tmp/kur.avi', pix_fmt='yuv420p')
+    .input('pipe:', format='h264', pix_fmt='yuv420p', s='{}x{}'.format(width, height))
+    .output('/tmp/kur.mp4', format='h264', pix_fmt='yuv420p')
+    .overwrite_output()
+    .run_async(pipe_stdin=True)
+)
+
+
+writer2 = (
+    ffmpeg
+    .input('pipe:', format='rawvideo', pix_fmt='yuv420p', s='{}x{}'.format(width, height))
+    .output('/tmp/kur.mp4', format='h264', pix_fmt='yuv420p')
     .overwrite_output()
     .run_async(pipe_stdin=True)
 )
@@ -28,4 +54,6 @@ writer = (
 while True:
     frame = reader.stdout.read(width * height * 3)
     print(len(frame))
-    writer.stdin.write(frame)
+    #encoder.stdin.write(frame)
+    encoded = frame  #encoder.stdout.read()
+    writer2.stdin.write(encoded)
