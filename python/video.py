b87a3f853aca15e93c71ffe51bba9e979b24cd45
Miroslav Vitkov <sir.vorac@gmail.com>
2025-04-14 18:48:52 +0300

    [py] Importig camera frames in python now works and with less code.
    Turns out already encoded h254 - although complex to decompose into frames - is easy to transmit, save or play as chunks.
    
    Next: think of a way to play that on the receiving side.

--- python/video.py
+++ python/video.py
@@ -9,38 +9,26 @@ import ffmpeg
 ### Public API.
 
 # cb(frame) - called on each frame
-def run(cb):
-    return None
+#def run(cb):
+#    return None
 
 
 ### Details.
 width = 640
 height = 480
-PIX_FMT='yuv420p'
-RESOLUTION = '{}x{}'.format(width, height)
-
-FORMAT = 'rawvideo'
-#FORMAT = 'h264'
 
 
 reader = (
     ffmpeg
-    .input('/dev/video0', s=RESOLUTION)
-    .output('pipe:', format=FORMAT, pix_fmt=PIX_FMT)
+    .input('/dev/video0', format='v4l2', s=f'{width}x{height}')
+    .output('pipe:', format='h264', vcodec='libx264', pix_fmt='yuv420p')
     .run_async(pipe_stdout=True)
 )
 
 
-writer = (
-    ffmpeg
-    .input('pipe:', format=FORMAT, pix_fmt=PIX_FMT, s=RESOLUTION)
-    .output('/tmp/kur.mp4', format='h264')
-    .overwrite_output()
-    .run_async(pipe_stdin=True)
-)
-
-
-while True:
-    frame = reader.stdout.read(width * height * 3)
-    print(len(frame))
-    writer.stdin.write(frame)
+with open('/tmp/kur.h264', 'wb') as out_file:
+    while True:
+        chunk = reader.stdout.read(4096)
+        print(len(chunk))
+        out_file.write(chunk)
+#reader.wait()
