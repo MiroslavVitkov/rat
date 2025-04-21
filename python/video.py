c0a3105a323620c59c2346f2f489933a2f445775
Miroslav Vitkov <sir.vorac@gmail.com>
2025-04-21 05:48:40 +0300

    [py] Refactor of video.py. Read settings from conf.ini .

--- python/video.py
+++ python/video.py
@@ -5,28 +5,22 @@
 #             - displaying frames one by one with mpv.
 
 
-import crypto
-
 import ffmpeg
 import subprocess
 
+import conf
+import crypto
 
-width = 640
-#width = 1920
-#height = 1080
-height = 480
-FRATE = 25
 
-### Public API.
 def capture():
     reader = (
         ffmpeg
-        .input('/dev/video0', format='v4l2', r=FRATE, s=f'{width}x{height}')
+        .input('/dev/video0', format='v4l2', r=conf[video][fps], s=conf[video][res])
         .output(
             'pipe:',
             format='h264',
             vcodec='libx264',
-            r=FRATE,
+            r=conf[video][fps],
             pix_fmt='yuv420p',
             preset='ultrafast',  # lowest compression!
             tune='zerolatency',
@@ -47,10 +41,10 @@ def watch( chunks ):
         '--no-demuxer-thread',
         '--demuxer-lavf-o=flv_format=h264',
         '--correct-pts=no',  #
-        '--container-fps-override=25',  #
+        '--container-fps-override=25', # warn
         '--opengl-glfinish=yes',  #
         '--opengl-swapinterval=0',  #
-        '--vo=xv',  #
+        '--vo=xv',  # warn
         '-'],
         stdin=subprocess.PIPE
     )
