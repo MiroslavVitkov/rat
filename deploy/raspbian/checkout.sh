78efdef7b4a2da311483a48a363a15bb7ba6e4b8
Miroslav Vitkov <sir.vorac@gmail.com>
2025-09-16 14:29:26 +0300

    [deploy] checkout.sh for deploying source from GitHub.
    Now the only step needed for CD are webhooks.

---
+++ deploy/raspbian/checkout.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+
+# In this file: update rat's source code with GitHub as source of truth.
+#               Notice .ssh/ and .git/ are not affected by the * operations.
+#               Make sure to have the repo cloned in https mode.
+#               Needs to be ran as root.
+
+
+set -u  # error on using uninitialized variable
+
+
+systemctl stop rat
+
+cd ~/proj/rat &&\
+git pull &&\
+python/rat.py test
+
+if [ $? -eq 0 ]; then
+    mv /opt/rat/conf.ini /tmp/
+    cp -r ~/proj/rat/* /opt/rat/
+    mv /tmp/conf.ini /opt/rat/
+fi
+
+systemctl start rat
