65d430a2de62cbe61c255d974243e1f0460df024
Miroslav Vitkov <sir.vorac@gmail.com>
2025-10-26 20:04:13 +0200

    [py] Placeholder file created: prot/media.py.

---
+++ python/prot/media.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+'''
+Conferencing.
+'''
+
+
+from impl import audio
+from impl import video
+
+
+def stream():
+    pass
+
+
+def watch():
+    pass
+
+
+def test():
+    print('prot/media.py: UNIT TESTS PASSED')
+
+
+if __name__ == '__main__':
+    test()
