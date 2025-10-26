4badbf3a2034804e003be420a152595cc8daef2c
Miroslav Vitkov <sir.vorac@gmail.com>
2025-10-26 21:25:22 +0200

    [py] media.py proof of concept: local TCP, non-encrypted, not played.

--- python/prot/media.py
+++ python/prot/media.py
@@ -6,16 +6,28 @@ Conferencing.
 '''
 
 
+from threading import Event
+
 from impl import audio
+from impl import conf
+from impl import sock
 from impl import video
 
 
-def stream():
-    pass
+def stream( death: Event=Event() ) -> None:
+    def foo( s: sock.socket, _ ):
+        for chunk in audio.stream():
+            s.sendall(chunk)
+
+    sock.Server(foo, conf.AUDIO)
 
 
 def watch():
-    pass
+    def foo( s:sock.socket ):
+        for chunk in sock.recv(s):
+            print(len(chunk))
+
+    sock.Client(foo, 'localhost', conf.AUDIO)
 
 
 def test():
